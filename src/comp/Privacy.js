f42351929537e1015d2cc6475401ff724731beae
advait <advait.suraj.vedant@gmail.com>
2022-12-12 20:44:10 -0800

    privacy done

--- src/comp/Privacy.js
+++ src/comp/Privacy.js
@@ -1,17 +1,35 @@
 import React from 'react'
-
+import Nav from './Nav'
+import Header from './Header'
+import ImgLineLong from './ImgLineLong'
+import ImgLineShort from './ImgLineShort'
+import { NavLink } from 'react-router-dom'
 function Privacy() {
   return (
-    <div class="flex items-center justify-center w-screen h-screen backdrop-blur-md z-50 fixed">
-        <div class="w-[80vw] h-[80vh] max-w-[40rem] max-h-[40rem] bg-white px-2 py-2 flex items-center justify-center flex-col">
+    <div class="w-screen flex flex-col h-screen overflow-hidden">
+    <Nav productname={"Swipe Wallet"}/>
+    <Header product={"Swipe Wallet"} phrase={["Made for you", " Built to impress"]}/>
+    <ImgLineLong/>
+    <ImgLineShort/>
+    <div id="privacy" class="flex items-center justify-center w-screen h-screen backdrop-blur-sm z-50 fixed">
+
+        <div class="w-[80vw] h-[80vh] max-w-[40rem] rounded-2xl max-h-[40rem] bg-white px-2 py-2 flex items-center justify-center flex-col">
             <span class="text-3xl font-lato text-center w-fit my-5">
                 Swipe Wallet Terms and Conditions
             </span>
             <span class="text-black font-lato w-[80%]">
-            This site uses cookies to personalise your experience, analyse site traffic and keep track of items stored in your shopping basket. By Clicking ACCEPT or continuing to browse the site you are agreeing to our use of cookies. See our Privacy Policy here.
+            This site uses cookies to personalise your experience, analyse site traffic and keep track of items stored in your shopping basket. By Clicking ACCEPT or continuing to browse the site you are agreeing to our use of cookies. See our <NavLink to="/Terms" activeStyle>
+            <span class="underline text-blue-500">Privacy Policy</span>
+          </NavLink> here.
+            </span>
+            <span class="bg-blue-500 px-5 py-1 rounded-3xl text-white font-semibold font-lato my-10">
+            <NavLink to="/home" activeStyle>
+                Accept
+            </NavLink>
             </span>
         </div>
-    </div>      
+    </div>     
+    </div> 
     
   )
 }
---
+++ src/comp/Terms.js
@@ -0,0 +1,107 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+function Terms() {
+  return (
+    <div class="flex flex-col bg-white w-[90vw] px-[5vw] py-[2vh] h-fit text-black font-lato">
+    Terms and Conditions<br/>
+    <br/>
+    <span class="text-3xl font-bold">Welcome to SwipeWallet!</span><br/>
+    <br/>
+    These terms and conditions outline the rules and regulations for the use of Swipe's Website, located at https://swipewallet.web.app/.<br/>
+    <br/>
+    By accessing this website, we assume you accept these terms and conditions. Do not continue to use SwipeWallet if you do not agree to take all of the terms and conditions stated on this page.<br/>
+    <br/>
+    <span class="text-3xl font-bold">Cookies:</span><br/>
+    The website uses cookies to help personalize your online experience. By accessing SwipeWallet, you agreed to use the required cookies.<br/>
+    <br/>
+    A cookie is a text file that is placed on your hard disk by a web page server. Cookies cannot be used to run programs or deliver viruses to your computer. Cookies are uniquely assigned to you and can only be read by a web server in the domain that issued the cookie to you.<br/>
+    <br/>
+    We may use cookies to collect, store, and track information for statistical or marketing purposes to operate our website. You have the ability to accept or decline optional Cookies. There are some required Cookies that are necessary for the operation of our website. These cookies do not require your consent as they always work. Please keep in mind that by accepting required Cookies, you also accept third-party Cookies, which might be used via third-party provided services if you use such services on our website, for example, a video display window provided by third parties and integrated into our website.<br/>
+    <br/>
+    <span class="text-3xl font-bold">License:</span><br/>
+    Unless otherwise stated, Swipe and/or its licensors own the intellectual property rights for all material on SwipeWallet. All intellectual property rights are reserved. You may access this from SwipeWallet for your own personal use subjected to restrictions set in these terms and conditions.<br/>
+    <br/>
+    <span class="text-3xl font-bold">You must not:</span><br/>
+    <br/>
+    Copy or republish material from SwipeWallet<br/>
+    Sell, rent, or sub-license material from SwipeWallet<br/>
+    Reproduce, duplicate or copy material from SwipeWallet<br/>
+    Redistribute content from SwipeWallet<br/>
+    This Agreement shall begin on the date hereof.<br/>
+    <br/>
+    Parts of this website offer users an opportunity to post and exchange opinions and information in certain areas of the website. Swipe does not filter, edit, publish or review Comments before their presence on the website. Comments do not reflect the views and opinions of Swipe, its agents, and/or affiliates. Comments reflect the views and opinions of the person who posts their views and opinions. To the extent permitted by applicable laws, Swipe shall not be liable for the Comments or any liability, damages, or expenses caused and/or suffered as a result of any use of and/or posting of and/or appearance of the Comments on this website.<br/>
+    <br/>
+    Swipe reserves the right to monitor all Comments and remove any Comments that can be considered inappropriate, offensive, or causes breach of these Terms and Conditions.<br/>
+    <br/>
+    <span class="text-3xl font-bold">You warrent and represent:</span><br/>
+    <br/>
+    You are entitled to post the Comments on our website and have all necessary licenses and consents to do so;<br/>
+    The Comments do not invade any intellectual property right, including without limitation copyright, patent, or trademark of any third party;<br/>
+    The Comments do not contain any defamatory, libelous, offensive, indecent, or otherwise unlawful material, which is an invasion of privacy.<br/>
+    The Comments will not be used to solicit or promote business or custom or present commercial activities or unlawful activity.<br/>
+    You hereby grant Swipe a non-exclusive license to use, reproduce, edit and authorize others to use, reproduce and edit any of your Comments in any and all forms, formats, or media.<br/>
+    <br/>
+    <span class="text-3xl font-bold">Hyperlinking to our Content:</span><br/>
+    The following organizations may link to our Website without prior written approval:<br/>
+    <br/>
+    Government agencies;<br/>
+    Search engines;<br/>
+    News organizations;<br/>
+    Online directory distributors may link to our Website in the same manner as they hyperlink to the Websites of other listed businesses; and<br/>
+    System-wide Accredited Businesses except soliciting non-profit organizations, charity shopping malls, and charity fundraising groups which may not hyperlink to our Web site.<br/>
+    These organizations may link to our home page, to publications, or to other Website information so long as the link: (a) is not in any way deceptive; (b) does not falsely imply sponsorship, endorsement, or approval of the linking party and its products and/or services; and (c) fits within the context of the linking party's site.<br/>
+    <br/>
+    We may consider and approve other link requests from the following types of organizations:<br/>
+    <br/>
+    commonly-known consumer and/or business information sources;<br/>
+    dot.com community sites;<br/>
+    associations or other groups representing charities;<br/>
+    online directory distributors;<br/>
+    internet portals;<br/>
+    accounting, law, and consulting firms; and<br/>
+    educational institutions and trade associations.<br/>
+    We will approve link requests from these organizations if we decide that: (a) the link would not make us look unfavorably to ourselves or to our accredited businesses; (b) the organization does not have any negative records with us; (c) the benefit to us from the visibility of the hyperlink compensates the absence of Swipe; and (d) the link is in the context of general resource information.<br/>
+    <br/>
+    These organizations may link to our home page so long as the link: (a) is not in any way deceptive; (b) does not falsely imply sponsorship, endorsement, or approval of the linking party and its products or services; and (c) fits within the context of the linking party's site.<br/>
+    <br/>
+    If you are one of the organizations listed in paragraph 2 above and are interested in linking to our website, you must inform us by sending an e-mail to Swipe. Please include your name, your organization name, contact information as well as the URL of your site, a list of any URLs from which you intend to link to our Website, and a list of the URLs on our site to which you would like to link. Wait 2-3 weeks for a response.<br/>
+    <br/>
+    Approved organizations may hyperlink to our Website as follows:<br/>
+    <br/>
+    By use of our corporate name; or<br/>
+    By use of the uniform resource locator being linked to; or<br/>
+    Using any other description of our Website being linked to that makes sense within the context and format of content on the linking party's site.<br/>
+    No use of Swipe's logo or other artwork will be allowed for linking absent a trademark license agreement.<br/>
+    <br/>
+    <span class="text-3xl font-bold">Content Liability:</span><br/>
+    We shall not be held responsible for any content that appears on your Website. You agree to protect and defend us against all claims that are raised on your Website. No link(s) should appear on any Website that may be interpreted as libelous, obscene, or criminal, or which infringes, otherwise violates, or advocates the infringement or other violation of, any third party rights.<br/>
+    <br/>
+    <span class="text-3xl font-bold">Reservation of Rights:</span><br/>
+    We reserve the right to request that you remove all links or any particular link to our Website. You approve to immediately remove all links to our Website upon request. We also reserve the right to amend these terms and conditions and its linking policy at any time. By continuously linking to our Website, you agree to be bound to and follow these linking terms and conditions.<br/>
+    <br/>
+    <span class="text-3xl font-bold">Removal of links from our website:</span><br/>
+    If you find any link on our Website that is offensive for any reason, you are free to contact and inform us at any moment. We will consider requests to remove links, but we are not obligated to or so or to respond to you directly.<br/>
+    <br/>
+    We do not ensure that the information on this website is correct. We do not warrant its completeness or accuracy, nor do we promise to ensure that the website remains available or that the material on the website is kept up to date.<br/>
+    <br/>
+    <span class="text-3xl font-bold">Disclaimer:</span><br/>
+    To the maximum extent permitted by applicable law, we exclude all representations, warranties, and conditions relating to our website and the use of this website. Nothing in this disclaimer will:<br/>
+    <br/>
+    limit or exclude our or your liability for death or personal injury;<br/>
+    limit or exclude our or your liability for fraud or fraudulent misrepresentation;<br/>
+    limit any of our or your liabilities in any way that is not permitted under applicable law; or<br/>
+    exclude any of our or your liabilities that may not be excluded under applicable law.<br/>
+    The limitations and prohibitions of liability set in this Section and elsewhere in this disclaimer: (a) are subject to the preceding paragraph; and (b) govern all liabilities arising under the disclaimer, including liabilities arising in contract, in tort, and for breach of statutory duty.<br/>
+    <br/>
+    As long as the website and the information and services on the website are provided free of charge, we will not be liable for any loss or damage of any nature.
+    <br/>
+    <br/>
+    <NavLink to="/" activeStyle>
+        <span class="text-3xl underline text-blue-500 font-lato flex flex-row justify-center text-center">back to the main page</span>
+    </NavLink></div>
+
+    
+  )
+}
+
+export default Terms
